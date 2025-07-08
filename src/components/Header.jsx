4e4a82d86ee095131a670eeaba39e2840ec55c9c
Mauricio Correa <correa.mauricio.david@gmail.com>
2025-07-08 06:35:11 -0500

    feature: Arreglar faltas de ortografía

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     <header className="header">
       <h1>Tienda on line de peliculas</h1>
       <nav>
-        <a href="/abouts">Acera de nosotros</a> |{" "}
+        <a href="/abouts">Acerca de nosotros</a> |{" "}
         <a href="/">Catálogo</a> |{" "}
         <a href="/rentals">Alquileres</a> |{" "}
         <a href="/purchases">Compras</a> |{" "}
